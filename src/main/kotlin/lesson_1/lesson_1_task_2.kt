d5212aa0a4421c234a06e746d063432aa9b6fb92
Aleksandr Golovko <alex-golovko@bk.ru>
2023-06-28 15:55:31 +0300

    KS-1-2 ready homework

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+fun main() {
+    val totalOrders = 75
+    val textThanks = "Спасибо за покупку"
+    println("$totalOrders\n$textThanks")
+    var workers = 2000
+    //  println(workers)
+    workers -= 1
+    println(workers)
+}
