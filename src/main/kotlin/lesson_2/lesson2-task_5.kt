c9ce77f9e1d834e3e4b04f017bce2cb5d890459d
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-02 13:20:50 +0300

    KS-2-5 ready homework (формула через pow())

--- src/main/kotlin/lesson_2/lesson2-task_5.kt
+++ src/main/kotlin/lesson_2/lesson2-task_5.kt
@@ -1,13 +1,12 @@
 package lesson_2
 
-fun main() {
-    var money = 70_000.0
-    val interestRate = 16.7
-    val years = 20
-    repeat(years) {
-        money += money / 100 * interestRate
-    }
-    money = ((money * 1000).toInt() / 1000.0)  //println( String.format("%.3f", money) )
-    println(money)
+import kotlin.math.pow
 
+fun main() {
+    val money = 70_000.0
+    val interestRate = 16.7
+    val years = 20.0
+    var money2 = money * (1 + interestRate / 100.00).pow(years)
+    money2 = ((money2 * 1000).toInt() / 1000.0)  //println( String.format("%.3f", money) )
+    println(money2)
 }
