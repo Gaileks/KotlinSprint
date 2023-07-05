7e6ca71f623697bb3c3de56990c7b28d0cb91419
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-05 14:14:56 +0300

    Исправил  имя переменных, второе условие

--- src/main/kotlin/lesson_4/lesson4-task_5.kt
+++ src/main/kotlin/lesson_4/lesson4-task_5.kt
@@ -1,23 +1,23 @@
 package lesson_4
 
 fun main() {
-    val hullDamage: Boolean
+    val isHullDamage: Boolean
     val crew: Int
     val provisions: Int
-    val goodWeather: Boolean
-    val readyToSail: Boolean
+    val isGoodWeather: Boolean
+    val isReadyToSail: Boolean
 
     println("Есть ли повреждения корпуса(true/false): ")
-    hullDamage = readln() == "true"
+    isHullDamage = readln() == "true"
     println("Текущая численность экипажа:")
     crew = readln().toInt()
     println("Текущее количество ящиков провизии:")
     provisions = readln().toInt()
     println("Благоприятные ли метеоусловия(true/false):")
-    goodWeather = readln() == "true"
+    isGoodWeather = readln() == "true"
 
-     readyToSail =
-        (hullDamage && crew > 55 && crew < 70 && provisions > 50) || (crew > 55 && crew == 70 && goodWeather)
+    isReadyToSail =
+        (isHullDamage && crew > 55 && crew < 70 && provisions > 50) || ( crew == 70 && isGoodWeather && provisions > 50)
 
-    println("Может ли корабль отправляться? $readyToSail ")
+    println("Может ли корабль отправляться? $isReadyToSail ")
 }
