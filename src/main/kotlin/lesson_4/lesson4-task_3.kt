857b4252ba5fcfacc872b6a8a0c79776a29fdbd3
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-05 13:52:56 +0300

    changed the name of variables

--- src/main/kotlin/lesson_4/lesson4-task_3.kt
+++ src/main/kotlin/lesson_4/lesson4-task_3.kt
@@ -2,11 +2,11 @@ package lesson_4
 
 fun main() {
 
-    val sunnyWeather = true
-    val awningOpen = true
+    val isSunnyWeather = true
+    val isAwningOpen = true
     val humidity = 20
     val season = "зима"
-    val goodConditions = sunnyWeather && awningOpen && (humidity == 20) && (season != "зима")
+    val isGoodConditions = isSunnyWeather && isAwningOpen && (humidity == 20) && (season != "зима")
 
-    println("Благоприятные ли условия сейчас для роста бобовых? $goodConditions")
+    println("Благоприятные ли условия сейчас для роста бобовых? $isGoodConditions")
 }
