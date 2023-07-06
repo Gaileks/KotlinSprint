b01cc57b370324bf204f724c376ab728c1a58c46
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-06 09:57:27 +0300

    !isHullDamage

--- src/main/kotlin/lesson_4/lesson4-task_5.kt
+++ src/main/kotlin/lesson_4/lesson4-task_5.kt
@@ -1,23 +1,19 @@
 package lesson_4
 
 fun main() {
-    val isHullDamage: Boolean
-    val crew: Int
-    val provisions: Int
-    val isGoodWeather: Boolean
     val isReadyToSail: Boolean
 
     println("Есть ли повреждения корпуса(true/false): ")
-    isHullDamage = readln() == "true"
+    val isHullDamage: Boolean = readln() == "true"
     println("Текущая численность экипажа:")
-    crew = readln().toInt()
+    val crew: Int = readln().toInt()
     println("Текущее количество ящиков провизии:")
-    provisions = readln().toInt()
+    val provisions: Int = readln().toInt()
     println("Благоприятные ли метеоусловия(true/false):")
-    isGoodWeather = readln() == "true"
+    val isGoodWeather: Boolean = readln() == "true"
 
     isReadyToSail =
-        (isHullDamage && crew > 55 && crew < 70 && provisions > 50) || ( crew == 70 && isGoodWeather && provisions > 50)
+        (!isHullDamage && crew > 55 && crew < 70 && provisions > 50) || ( crew == 70 && isGoodWeather && provisions > 50)
 
     println("Может ли корабль отправляться? $isReadyToSail ")
 }
