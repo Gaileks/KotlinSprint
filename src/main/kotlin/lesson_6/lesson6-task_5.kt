4593051a0f12fe4afc76f86a64e153ab15519f7a
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-11 15:05:53 +0300

    KS-6-5 ready homework

--- src/main/kotlin/lesson_6/lesson6-task_5.kt
+++ src/main/kotlin/lesson_6/lesson6-task_5.kt
@@ -5,13 +5,16 @@ fun main() {
     do {
         val numOne = (1..9).random()
         val numTwo = (1..9).random()
-        println("Напишите правильный ответ $numOne + $numTwo = ")
         val answer = numOne + numTwo
+        println("Напишите правильный ответ $numOne + $numTwo = ")
+
+
         if (readln().toInt() == answer) {
             println("Добро пожаловать!")
             count = 0
         } else {
             println("Доступ запрещен! Осталось попыток ${--count} ")
         }
+
     } while (count > 0)
 }
