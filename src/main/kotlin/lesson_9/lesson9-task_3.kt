ac098f8248a0df6796257f78edda7531f8214f27
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-13 20:06:15 +0300

    KS-9-3 ready homework

---
+++ src/main/kotlin/lesson_9/lesson9-task_3.kt
@@ -0,0 +1,13 @@
+package lesson_9
+
+fun main() {
+    val recipeIngredients = mutableListOf(2, 50, 15)
+
+    println("Введите количество порций:")
+
+    val numberServings = readln().toInt()
+    val recipeIngredientsTwo = recipeIngredients.map { it * numberServings }
+
+    println("На $numberServings порций вам понадобится:")
+    println(" Яиц – ${recipeIngredientsTwo[0]} шт., молока – ${recipeIngredientsTwo[1]} мл., сливочного масла – ${recipeIngredientsTwo[2]} гр.")
+}
