3416fc1fb6774de876d843009097a74ae3935227
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-13 17:46:11 +0300

    KS-8-3 ready homework

--- src/main/kotlin/lesson_8/lesson8-task_3.kt
+++ src/main/kotlin/lesson_8/lesson8-task_3.kt
@@ -1,10 +1,10 @@
 package lesson_8
 
 fun main() {
-    val recipe = arrayOf("Молоко", "Яйца", "Мука", "Сахар", "Соль")
-    val ingredient = readln().lowercase().capitalize()
+    val recipeIngredients = arrayOf("молоко", "яйца", "мука", "сахар", "соль")
+    val ingredient = readln().lowercase()
 
-    if (ingredient in recipe) {
+    if (ingredient in recipeIngredients) {
         println("Ингредиент $ingredient. в рецепте есть")
     } else println("Такого ингредиента в рецепте нет")
 }
