682c26ec36a5ca5f9c5012aefc4be32c8df3329b
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-13 17:47:18 +0300

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
+    println("Hello World!".cyan())
 }
+fun String.cyan() = "\u001b[36m${this}\u001b[0m"
+fun String.white() = "\u001b[60m${this}\u001b[0m"
+fun String.black() = "\u001b[30m${this}\u001b[0m"
+fun String.bgWhite() = "\u001b[40m${this}\u001b[0m"
+fun String.brightWhite() = "\u001b[31;1m${this}\u001b[0m"
+fun String.bold() = "\u001b[1m${this}\u001b[0m"
