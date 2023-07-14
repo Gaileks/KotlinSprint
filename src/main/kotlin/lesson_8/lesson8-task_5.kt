32793e11e9781edc5367b30f4505d1682a21fb5b
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-14 12:32:48 +0300

    KS-8-5 ready homework

--- src/main/kotlin/lesson_8/lesson8-task_5.kt
+++ src/main/kotlin/lesson_8/lesson8-task_5.kt
@@ -3,8 +3,7 @@ package lesson_8
 fun main() {
     println("Сколько ингредиентов будет в рецепте:")
 
-    val recipeLength = readln().toInt()
-    val recipeIngredients = arrayOfNulls<String>(recipeLength)
+    val recipeIngredients = arrayOfNulls<String>(readln().toInt())
 
     for (i in recipeIngredients.indices) {
         println("Добавьте ${i + 1} ингредиент в рецепт: ")
