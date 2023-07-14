4f35a5829aeb1fda8787442a680a7b1636d20204
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-14 22:00:12 +0300

    KS-9-4 ready homework

--- src/main/kotlin/lesson_9/lesson9-task_4.kt
+++ src/main/kotlin/lesson_9/lesson9-task_4.kt
@@ -2,7 +2,7 @@ package lesson_9
 
 fun main() {
     println("Введите ингредиенты рецепта (через запятую): ")
-    val recipeIngredients = readln().split(",").map { it.trim() }.take(5).sorted()
+    val recipeIngredients = readln().split(",").map { it.trim() }.sorted()
 
     println(recipeIngredients)
 }
