acb6cd0d14d5468c1cd3e6b2d104bf2156cd5774
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-26 16:57:04 +0300

    KS-11-1 ready homework

---
+++ src/main/kotlin/lesson_11/lesson_11_1/lesson11-task_1.kt
@@ -0,0 +1,10 @@
+package lesson_11.lesson_11_1
+
+    fun main() {
+
+    val user1 = User(1, "логин1", "пароль1", "mail1")
+    val user2 = User(2, "логин2", "пароль2", "mail2")
+    user1.getInfo()
+    user2.getInfo()
+
+    }
