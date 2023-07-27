383100924c0d906043510e47f0dc65f1ff044360
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-27 14:08:03 +0300

    KS-11-1 ready homework

--- src/main/kotlin/lesson_11/lesson_11_1/lesson11-task_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_1/lesson11-task_1.kt
@@ -1,10 +1,30 @@
 package lesson_11.lesson_11_1
 
-    fun main() {
+fun main() {
 
-    val user1 = User(1, "логин1", "пароль1", "mail1")
-    val user2 = User(2, "логин2", "пароль2", "mail2")
-    user1.getInfo()
-    user2.getInfo()
+    val userOne = User(1, "логин1", "пароль1", "mail1")
+    val userTwo = User(2, "логин2", "пароль2", "mail2")
+    userOne.getInfo()
+    userTwo.getInfo()
 
+}
+
+class User(
+    private var id: Int,
+    private var login: String,
+    private var password: String,
+    private var mail: String,
+) {
+
+    fun getInfo() {
+        println(
+            """
+    id       = $id
+    login    = $login
+    password = $password
+    mail     = $mail
+    _________________          
+      """.trimIndent()
+        )
     }
+}
