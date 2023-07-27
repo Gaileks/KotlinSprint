5945af95d46350631f56f89693e4e93989dc2100
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-27 14:18:35 +0300

    KS-11-2 ready homework

--- src/main/kotlin/lesson_11/lesson_11_2/lesson11-task_2.kt
+++ src/main/kotlin/lesson_11/lesson_11_2/lesson11-task_2.kt
@@ -3,10 +3,58 @@ package lesson_11.lesson_11_2
 
 fun main() {
 
-    val user1 = User(1, "Алекс", "1111", "mail")
+    val userOne = User(1, "Алекс", "1111", "mail")
 
-    user1.setBio()
-    user1.changePassword()
-    user1.getInfo()
-    user1.sendEmail(user1.getPassword())
+    userOne.setBio()
+    userOne.changePassword()
+    userOne.getInfo()
+    userOne.sendEmail(userOne.getPassword())
+}
+
+class User(
+    private val id: Int,
+    private val login: String,
+    private var password: String,
+    private val mail: String,
+    private var bio: String = ""
+) {
+
+    fun getPassword() = password
+
+    fun sendEmail(text: String) {
+        println("Здравствуйте $login ваш новый пароль $text")
+    }
+
+    fun setBio() {
+        println("Добавьте информацию о себе:")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        do {
+            println("Для изменения паролья введите текущий пароль:")
+            val userPassword = readln()
+
+            if (userPassword == password) {
+                println("Введите новый пароль")
+                password = readln()
+                return
+            } else {
+                println("Неправильный пароль")
+            }
+        } while (true)
+    }
+
+    fun getInfo() {
+        println(
+            """
+    id       = $id
+    login    = $login
+    password = $password
+    mail     = $mail
+    bio      = $bio
+    _________________          
+      """.trimIndent()
+        )
+    }
 }
