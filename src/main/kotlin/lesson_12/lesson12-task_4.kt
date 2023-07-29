62f232e30f144af3fd49789124e5873bdb4b112e
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-29 20:52:34 +0300

    KS-12-4 ready homework

--- src/main/kotlin/lesson_12/lesson12-task_4.kt
+++ src/main/kotlin/lesson_12/lesson12-task_4.kt
@@ -6,7 +6,6 @@ fun main() {
     val weatherOne = WeatherDay(15, 2, 700)
     val weatherTwo = WeatherDay(4, -5, 750, false)
     val weatherThree = WeatherDay(30, 10, 730)
-
 }
 
 class WeatherDay(
@@ -14,7 +13,6 @@ class WeatherDay(
     nightTemperature: Int,
     atmosphericPressure: Int,
     wasItRaining: Boolean = true
-
 ) {
 
     init {
