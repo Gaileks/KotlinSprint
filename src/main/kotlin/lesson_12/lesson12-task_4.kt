e60aede04afc169f3780c2086af73016ac11eb87
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-29 20:42:53 +0300

    KS-12-4 ready homework

---
+++ src/main/kotlin/lesson_12/lesson12-task_4.kt
@@ -0,0 +1,38 @@
+package lesson_12
+
+
+fun main() {
+
+    val weatherOne = WeatherDay(15, 2, 700)
+    val weatherTwo = WeatherDay(4, -5, 750, false)
+    val weatherThree = WeatherDay(30, 10, 730)
+
+}
+
+class WeatherDay(
+    daytimeTemperature: Int,
+    nightTemperature: Int,
+    atmosphericPressure: Int,
+    wasItRaining: Boolean = true
+
+) {
+
+    init {
+        println(
+            """
+                Погода: 
+                Температура днем: $daytimeTemperature
+                Температура ночью: $nightTemperature  
+                Атмосферное давление: $atmosphericPressure
+                Осадки, дождь: ${
+                if (wasItRaining) {
+                    "да"
+                } else {
+                    "нет"
+                }
+            }
+                _________________________
+            """.trimIndent()
+        )
+    }
+}
