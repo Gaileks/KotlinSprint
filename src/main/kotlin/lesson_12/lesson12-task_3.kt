be1632d94cc7ff81ac1282fd6343616c10070995
Aleksandr Golovko <alex-golovko@bk.ru>
2023-07-30 15:21:54 +0300

    KS-12-4 ready homework

--- src/main/kotlin/lesson_12/lesson12-task_3.kt
+++ src/main/kotlin/lesson_12/lesson12-task_3.kt
@@ -1,15 +1,15 @@
 package lesson_12
 
-import lesson_12.WeatherDay.Companion.getWeatherByDay
-
 fun main() {
 
     val weatherOne = WeatherDay(15, 2, 700)
     val weatherTwo = WeatherDay(4, -5, 750, false)
     val weatherThree = WeatherDay(30, 10, 730)
-    val weatherByDay = listOf<WeatherDay>(weatherOne, weatherTwo, weatherThree)
+    val weatherByDay = listOf(weatherOne, weatherTwo, weatherThree)
 
-    getWeatherByDay(weatherByDay)
+    weatherByDay.forEach(){
+       it.getWeatherByDay()
+    }
 }
 
 class WeatherDay(
@@ -17,29 +17,25 @@ class WeatherDay(
     private var nightTemperature: Int,
     private var atmosphericPressure: Int,
     private var wasItRaining: Boolean = true
-
 ) {
-    companion object {
-        fun getWeatherByDay(days: List<WeatherDay>) {
-            var count = 0
-            days.forEach() {
-                println(
-                    """
-                День ${++count} погода: 
-                Температура днем: ${it.daytimeTemperature}
-                Температура ночью: ${it.nightTemperature}  
-                Атмосферное давление: ${it.atmosphericPressure}
+
+    fun getWeatherByDay() {
+            println(
+                """
+                Погода: 
+                Температура днем: $daytimeTemperature
+                Температура ночью: $nightTemperature  
+                Атмосферное давление: $atmosphericPressure
                 Осадки, дождь: ${
-                        if (it.wasItRaining) {
-                            "да"
-                        } else {
-                            "нет"
-                        }
+                    if (wasItRaining) {
+                        "да"
+                    } else {
+                        "нет"
                     }
+                }
                 _________________________
             """.trimIndent()
-                )
-            }
+            )
         }
     }
-}
+
