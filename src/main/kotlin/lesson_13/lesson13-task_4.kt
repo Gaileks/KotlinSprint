8cc5b86c92b7798ddab8bb0a4130ec0d73d183f4
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-01 11:09:31 +0300

    KS-13-4 ready homework

---
+++ src/main/kotlin/lesson_13/lesson13-task_4.kt
@@ -0,0 +1,49 @@
+package lesson_13
+
+fun main() {
+    val userCards = addUserCard()
+
+    userCards.forEach { it.getContactInformation() }
+}
+
+fun addUserCard(): List<UserCardTwo> {
+    val userCards = mutableListOf<UserCardTwo>()
+    var str = "да"
+    var name: String?
+    var phoneNumber: Long?
+    var company: String?
+
+    do {
+        println("Для добаления нового пользователя введите его данные:\nИмя: ")
+        name = readln()
+        println("№ телефона:")
+        phoneNumber = readln().toLongOrNull()
+
+        if (phoneNumber == null) {
+            println("№ телефона это обязательный параметр, допускаются только цифры, введите контакт заново")
+            continue
+        }
+
+        println("Название компании:")
+        company = readln()
+
+        if (company.isEmpty()) company = null
+        if (name.isEmpty()) name = null
+
+        userCards.add(UserCardTwo(name, phoneNumber, company))
+        println("Если хотите добавить новую запись, введите \"да\"")
+        str = readln().lowercase()
+
+    } while (str == "да")
+    return userCards
+}
+
+class UserCardTwo(
+    private var name: String?,
+    private var phoneNumber: Long?,
+    private var company: String?
+) {
+    fun getContactInformation() {
+        println("Имя: $name, Номер: $phoneNumber, Компания: $company")
+    }
+}
