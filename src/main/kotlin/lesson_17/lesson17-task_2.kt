f654945a0048d0967f4fac94d749c001a1ec35f8
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-07 13:29:17 +0300

    KS-17-2 ready homework

---
+++ src/main/kotlin/lesson_17/lesson17-task_2.kt
@@ -0,0 +1,21 @@
+package lesson_17
+
+fun main() {
+
+    val vessel = Vessel("Тихий", 12, "Большой порт Санкт-Петербург")
+
+    vessel.name = "Ловкий"
+    println(vessel.name)
+}
+
+class Vessel(
+    name: String,
+    averageSpeed: Int,
+    homePort: String
+) {
+    var name = name
+        set(value) {
+            println("Внимание, имя у корабля менять нельзя!")
+            field = "Тихий"
+        }
+}
