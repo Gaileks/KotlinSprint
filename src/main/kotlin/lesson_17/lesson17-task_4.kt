7d7b6074dc3fb05b24290c64597f574259fd51ed
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-08 20:07:43 +0300

    KS-17-4 ready homework

--- src/main/kotlin/lesson_17/lesson17-task_4.kt
+++ src/main/kotlin/lesson_17/lesson17-task_4.kt
@@ -3,17 +3,13 @@ package lesson_17
 fun main() {
     val thePackage = ThePackage("Москва", 12)
 
-    thePackage.trackNumber = 24
     thePackage.location = "Питер"
     thePackage.location = "Лондон"
     thePackage.getInfoPackage()
 }
 
-class ThePackage(location: String, trackNumber: Int) {
-
+class ThePackage(location: String, private val trackNumber: Int) {
     private var movementCounter = 0
-    var trackNumber = trackNumber
-        set(value) {}
 
     var location = location
         set(value) {
