e5e064ea674fca15b36173553cc8d96bef7b4104
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-08 19:46:57 +0300

    KS-17-3 ready homework

--- src/main/kotlin/lesson_17/lesson17-task_3.kt
+++ src/main/kotlin/lesson_17/lesson17-task_3.kt
@@ -2,27 +2,27 @@ package lesson_17
 
 fun main() {
 
-    val folders = Folders()
+    val folders = Folder(10, "Системные файлы ")
 
     println(folders.name + folders.numberOfFiles)
     folders.secret = false
     println(folders.name + folders.numberOfFiles)
 }
 
-class Folders {
+class Folder(numberOfFiles: Int, name: String) {
     var secret: Boolean = true
-    var name: String = "Системные файлы "
-        get() {
-            return if (secret) {
-                "Скрытая папка "
-            } else field
+    var name = name
+        get() = when {
+            secret -> "Скрытая папка "
+            else -> field
         }
-    var numberOfFiles: Int = 10
-        get() {
-            return if (secret) {
-                0
-            } else field
+    var numberOfFiles = numberOfFiles
+        get() = when {
+            secret -> 0
+            else -> field
         }
+
+
 }
 
 
