e328908b08bc408b02cf8cf58df177f80f5081ed
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-10 17:52:37 +0300

    KS-19-4 ready homework

---
+++ src/main/kotlin/lesson_19/lesson19-task_4.kt
@@ -0,0 +1,35 @@
+package lesson_19
+
+fun main() {
+
+    val tank = Tank()
+    tank.makeShot()
+    tank.getWeaponNewType(Ammunition.BLUE)
+    tank.makeShot()
+    tank.getWeaponNewType(Ammunition.RED)
+    tank.makeShot()
+}
+
+enum class Ammunition(val impactForce: Int) {
+    BLUE(5),
+    GREEN(10),
+    RED(20),
+}
+
+class Tank(
+    private var weapon: Ammunition? = null
+) {
+
+    fun getWeaponNewType(weapon: Ammunition) {
+        println("Теперь танк заряжен снарядом ${weapon.name} ")
+        this.weapon = weapon
+    }
+
+    fun makeShot() {
+        if (weapon != null) {
+            println("Танк произвел выстрел снарядом ${weapon!!.name} сила урона ${weapon!!.impactForce} ")
+        } else {
+            println("У танка патронов нет")
+        }
+    }
+}
