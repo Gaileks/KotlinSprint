b483dffd39fe7b39372a273342c62d1bc1a4f712
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-11 16:27:55 +0300

    KS-17-5 ready homework

--- src/main/kotlin/lesson_17/lesson17-task_5.kt
+++ src/main/kotlin/lesson_17/lesson17-task_5.kt
@@ -2,24 +2,23 @@ package lesson_17
 
 fun main() {
 
-    val chatUser = ChatUser("Приз")
+    val chatUser = ChatUser("Приз","&%^(f")
 
     chatUser.login = "Машина"
     chatUser.password = "12"
-    chatUser.password
+    println(chatUser.password)
 }
 
-class ChatUser(login: String) {
+class ChatUser(login: String, password: String) {
 
     var login = login
         set(value) {
             println("Вы успешно поменяли свой логин")
             field = value
         }
-    var password = "&*57"
-        get() = repeat(field.length) {
-            print("*")
-        }.toString()
+    var password = password
+        get() = "".padEnd(field.length, '*')
+
         set(value) {
             println("Вы не можете изменить пароль")
         }
