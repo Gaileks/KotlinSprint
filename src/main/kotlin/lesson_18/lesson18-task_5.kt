3bf68663afcfe988c3d2b3345547b1208f54c1f4
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-16 13:38:40 +0300

    KS-18-5 ready homework

--- src/main/kotlin/lesson_18/lesson18-task_5.kt
+++ src/main/kotlin/lesson_18/lesson18-task_5.kt
@@ -5,28 +5,36 @@ fun main() {
 
     val string = Screen()
 
-    string.draw(PointNew(x = 1, y = 1))
-    string.draw(SquareNew(x = 2f, y = 2f))
-    string.draw(CircleNew(x = 3, y = 3f))
+    string.draw(PointNew(), 1, 1)
+    string.draw(SquareNew(), 2f, 2f)
+    string.draw(CircleNew(), 3, 3f)
+    string.draw(CircleNew(), 4f, 4)
+
 }
 
 class Screen {
 
-    fun draw(figure: Figure) {
-        println("Нарисовали ${figure.name}, координаты X:${figure.x}, Y:${figure.y} ")
+    fun draw(figure: Figure, x: Float, y: Int) {
+        println("Фигура ${figure.name} нарисована в координатах $x и $y")
+    }
+
+    fun draw(figure: Figure, x: Int, y: Int) {
+        println("Фигура ${figure.name} нарисована в координатах $x и $y")
+    }
+
+    fun draw(figure: Figure, x: Int, y: Float) {
+        println("Фигура ${figure.name} нарисована в координатах $x и $y")
+    }
+
+    fun draw(figure: Figure, x: Float, y: Float) {
+        println("Фигура ${figure.name} нарисована в координатах $x и $y")
     }
 }
 
-abstract class Figure(
-    open val name: String,
-    open val x: Any,
-    open val y: Any,
-)
+abstract class Figure(open val name: String)
 
+class PointNew(override val name: String = "Точка") : Figure(name)
 
-class PointNew(override val name: String = "Точка", override val x: Int, override val y: Int) : Figure(name, x, y)
+class SquareNew(override val name: String = "Квадрат") : Figure(name)
 
-class SquareNew(override val name: String = "Квадрат", override val x: Float, override val y: Float) :
-    Figure(name, x, y)
-
-class CircleNew(override val name: String = "Круг", override val x: Int, override val y: Float) : Figure(name, x, y)
+class CircleNew(override val name: String = "Круг") : Figure(name)
