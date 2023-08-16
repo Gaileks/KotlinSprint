0b38c8bd0db59bb839d3bbef986a0a47ecd37235
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-16 13:10:24 +0300

    KS-20-4 ready homework

--- src/main/kotlin/lesson_20/lesson20-task_4.kt
+++ src/main/kotlin/lesson_20/lesson20-task_4.kt
@@ -3,13 +3,10 @@ package lesson_20
 fun main() {
 
     val list = mutableListOf(
-        "1", "2", "3", "4",
-        "5", "6", "7", "8",
-        "9", "10", "11", "12"
-    ).map { { println("Нажат элемент $it") } }
-
-    for (i in 3..list.size step 4) {
-        list[i]()
-    }
+        "0", "1", "2", "3",
+        "4", "5", "6", "7",
+        "8", "9", "10", "11"
+    ).map { { println("Нажат элемент $it") } }.filterIndexed { index, _ -> index % 2 == 0 }.map { it() }
 }
+
 
